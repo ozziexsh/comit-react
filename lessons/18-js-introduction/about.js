317883ec56f79e8c28de1017e1d8b314b5f5f8c3
huongntt13 <huongntt13@gmail.com>
2019-10-15 18:45:44 -0600

    Lesson 18

---
+++ lessons/18-js-introduction/about.js
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>About Page</title>
+        <body>
+            <script>
+            //Show a popup Hello World
+            alert("This is the about page")
+        
+            </script>
+        </body>
+    </head>
+</html>
