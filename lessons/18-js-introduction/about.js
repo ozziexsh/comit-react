ea1c676119520c7e1e75e3b849acf1475f7fd3af
huongntt13 <huongntt13@gmail.com>
2019-10-15 19:43:33 -0600

    Completed lesson 19

--- lessons/18-js-introduction/about.js
+++ lessons/18-js-introduction/about.js
@@ -1,12 +1,2 @@
-<html>
-    <head>
-        <title>About Page</title>
-        <body>
-            <script>
-            //Show a popup Hello World
-            alert("This is the about page")
-        
-            </script>
-        </body>
-    </head>
-</html>
+let firstName ="Ozzie";
+console.log("First Name: ",firstName );
---
+++ lessons/19-js-variables/index.js
@@ -0,0 +1,17 @@
+const monthOfBirth ="August";
+const firstName ="Ozzie";
+const lastName ="Neher";
+let city;
+let age;
+let nextCity;
+
+city ="Saskatoon";
+age = 55;
+nextCity = city;
+console.log("Month of birth: ", monthOfBirth);
+console.log("First Name: ", firstName);
+console.log("Last Name: ", lastName);
+console.log("City: ", city);
+console.log("Age: ", age);
+console.log("Next city: ", nextCity);
+console.log("Full name: ", `${firstName}`)
