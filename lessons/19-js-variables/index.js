f137d45646a7e4f7b66116992abd6dd42ddeebc4
Mital Patel <mitu0309@gmail.com>
2019-10-15 19:59:55 -0600

    19-js-variable

---
+++ lessons/19-js-variables/index.js
@@ -0,0 +1,17 @@
+const monthOfBirth="september";
+const firstName="Mital";
+const lastName="patel";
+
+let city;
+let age;
+let newCity;
+
+city="saskatoon";
+age=33;
+newCity=city;
+console.log("Month of Birth",monthOfBirth);
+console.log("first name:",firstName);
+console.log("last name:",lastName);
+console.log("City:",city);
+console.log("Age",age);
+console.log("New City:",newCity);
