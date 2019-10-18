286238cbe0a1bd65f63d1d16001a4c824f974181
Mital Patel <mitu0309@gmail.com>
2019-10-17 18:54:41 -0600

    21-js-comparison

---
+++ lessons/21-js-comparison/index.js
@@ -0,0 +1,6 @@
+
+alert("Enter two numbers to see if the first is greater than the second")
+const fn=prompt(" Enter first number");
+const sn=prompt(" Enter first number");
+result=fn >sn;
+const result=confirm("were we true",result);
