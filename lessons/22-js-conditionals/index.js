4248270ddedebcea005508682bdfa2a0806b9d1e
roylyh <roy.louyihui@gmail.com>
2019-10-17 22:45:19 -0600

    Completed lesson 22

---
+++ lessons/22-js-conditionals/index.js
@@ -0,0 +1,12 @@
+alert("Enter two numbers to see if the first is greater than the second");
+
+let num1=prompt("enter the first number");
+let num2=prompt("enter the second number");
+
+if (num1>num2) {
+    alert("The first number is greater than the second number");
+} else if (num1<num2) {
+    alert("The first number is less than the second number");
+} else {
+    alert("The first number is the same as the second number");
+}
