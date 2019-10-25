65a167f4c57b874c7c9ab1fb192b9bf72aa9df15
Huswat-prog <huswatolanite@gmail.com>
2019-10-24 19:53:24 -0600

    done chapter 19

---
+++ lessons/19-js-variables/index.js
@@ -0,0 +1,19 @@
+const monthOfBirth="August"
+const firstName="Huswat"
+const lastName="Olanite"
+
+let city;
+let age;
+let nextCity;
+
+city="saskatoon"
+age=30
+nextCity=city
+
+console.log("Month of Birth:",monthOfBirth);
+console.log("First Name:",firstName);
+console.log("Last  Name:",lastName);
+console.log("City:",city);
+console.log("Age:",age);
+console.log("Next city:",nextCity);
+console.log("Full name:",`${firstName} ${lasttName}`);
