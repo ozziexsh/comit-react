144f8cb81d2ad0323166f4f9f61c873601127a53
Huswat-prog <huswatolanite@gmail.com>
2019-10-28 20:12:32 -0600

    Completes 20-js-converse

---
+++ lessons/20-js-conversions/index.js
@@ -0,0 +1,26 @@
+var age =20;
+var name="Huswah";
+var programStatus=false;
+var finalScore=null;
+var winningNumbers=undefined
+
+
+var age ="20"
+var name="Huswah";
+var programStatus=( Boolean(0) );
+var finalScore=( Boolean(0) );
+var winningNumbers=undefined
+
+
+console.log(age) 
+console.log(name)
+console.log(status)
+console.log(finalScore)=
+console.log(winningNumbers)
+
+
+console.log(age) 
+console.log(name)
+console.log(status)
+console.log(finalScore)=
+console.log(winningNumbers)
